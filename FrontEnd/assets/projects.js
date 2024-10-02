036454ae283f3eccbd0f4186a150c6d4cb9f674b
roCal93 <romaincalmelet@gmail.com>
2024-10-02 17:13:16 +0200

    displayProjectsDone

--- FrontEnd/assets/projects.js
+++ FrontEnd/assets/projects.js
@@ -7,9 +7,36 @@ if (projects === null) {
     projects = await reponse.json();
     // Transform format projects into JSON
     const jsonProjects = JSON.stringify(projects);
-    // put projects in the local storage 
+    // Put projects in the local storage 
     window.localStorage.setItem("projects", jsonProjects); 
 } else {
+    // Takes a JSON string and transforms it into a JavaScript object
     projects = JSON.parse(projects); {}
 }
 
+console.log(projects)
+
+//function to display the different projects
+function displayProjects(projects){
+    for(let i = 0; i < projects.length; i++) {
+        
+        const  allProject = projects[i];
+        // Retrieve the DOM element that will host the projects
+        const divGallery = document.querySelector(".gallery");
+        // Creating an element dedicated to a project
+        const projectElement = document.createElement("figure");
+        projectElement.dataset.id = projects[i].id;
+        // Creating elements
+        const imageElement = document.createElement("img");
+		imageElement.src = allProject.imageUrl;
+        imageElement.alt = allProject.title;
+        const titleElement = document.createElement("figcaption");
+        titleElement.innerText = allProject.title;
+
+        divGallery.appendChild(projectElement);
+        projectElement.appendChild(imageElement);
+        projectElement.appendChild(titleElement);
+    }
+}
+
+displayProjects(projects)
