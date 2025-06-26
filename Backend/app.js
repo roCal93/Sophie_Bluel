0a004a432fb9b8a4c3776aa0c5d0a4235a8d1124
Romain Calmelet <romaincalmelet@gmail.com>
2025-06-26 13:57:06 +0200

    deploy

--- Backend/app.js
+++ Backend/app.js
@@ -11,17 +11,22 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(helmet({
-      crossOriginResourcePolicy: false,
-    }));
+  crossOriginResourcePolicy: false,
+}));
 app.use('/images', express.static(path.join(__dirname, 'images')))
 
 const db = require("./models");
 const userRoutes = require('./routes/user.routes');
 const categoriesRoutes = require('./routes/categories.routes');
 const worksRoutes = require('./routes/works.routes');
-db.sequelize.sync().then(()=> console.log('db is ready'));
+db.sequelize.sync().then(() => console.log('db is ready'));
 app.use('/api/users', userRoutes);
 app.use('/api/categories', categoriesRoutes);
 app.use('/api/works', worksRoutes);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 module.exports = app;
+
+const port = process.env.PORT || 5678;
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
